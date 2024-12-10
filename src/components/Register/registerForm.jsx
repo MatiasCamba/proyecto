2fefcf80db52e62824ac2328c2aaf3abb22e3fe7
matiascamba <matiezequielcamba@gmail.com>
2024-12-09 21:00:02 -0300

    feature/RegisterForm: Register initiated

---
+++ src/components/Register/registerForm.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const registerForm = () => {
+  return (
+    <div>
+      <label htmlFor=""></label>
+      <input type="text" />
+      <label htmlFor=""></label>
+      <input type="text" />
+      <label htmlFor=""></label>
+      <input type="text" />
+      <label htmlFor=""></label>
+      <input type="text" />
+      <button>Enviar</button>
+    </div>
+  );
+};
+
+export default registerForm;
