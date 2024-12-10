4ab8f1450d75fdfa323fae737d3de6e1fd296548
matiascamba <matiezequielcamba@gmail.com>
2024-12-09 22:00:26 -0300

    listo form

--- src/components/Register/registerForm.jsx
+++ src/components/Register/registerForm.jsx
@@ -1,17 +1,23 @@
 import React from "react";
+import { useState } from "react";
 
 const registerForm = () => {
+  const [nombre,setNombre] = useState('');
+
   return (
     <div>
       <label htmlFor=""></label>
       <input type="text" />
       <label htmlFor=""></label>
+
+
       <input type="text" />
       <label htmlFor=""></label>
       <input type="text" />
       <label htmlFor=""></label>
       <input type="text" />
       <button>Enviar</button>
+      <button>No enviar</button>
     </div>
   );
 };
